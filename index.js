ba9ebaa2a4390da42c7b214bf92c7de3e3a5f83b
Nirav Soni <soninirav910@gmail.com>
2023-01-25 20:09:36 +0530

    Rest operator

--- index.js
+++ index.js
@@ -747,11 +747,9 @@ const circle = {
 //     console.log('run');
 // };
 
-function sum(){
-    let total = 0;
-    for(let value of arguments)
-    total+= value;
-    return total;
+function sum(discount,...args){
+    const total = args.reduce((a, b) => a + b);
+    return total * (1 - discount);
 }
 
-console.log(sum(1, 2, 3, 4, 5));
+console.log(sum(0.1, 20, 30));
