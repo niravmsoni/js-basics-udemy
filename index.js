bc26c3c9526b27cfa2477d927d0e0c4938ef1c6c
Nirav Soni <soninirav910@gmail.com>
2023-01-26 19:18:51 +0530

    Try catch handling

--- index.js
+++ index.js
@@ -778,13 +778,23 @@ const person = {
         return `${person.firstName} ${person.lastName}`
     },
     set fullName(value){
+        if (typeof value !== 'string')
+            throw new Error('Value is not a string.');
+
         const parts = value.split(' ');
+        if (parts.length !== 2)
+            throw new Error('Enter a first and last name')
+
         this.firstName = parts[0];
         this.lastName = parts[1];
     }
 };
 
-console.log(person.fullName);
-person.fullName = 'Hemali Soni';
+try{
+    person.fullName = '';
+}
+catch(e){
+    alert(e);
+}
 
 console.log(person);
